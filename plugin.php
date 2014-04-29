a6e33646391e0d98f15aa31783e61fe860a7e095
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-04-29 09:00:12 +0200

    Show Share controls when user is not logged in. Fixed #1

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ if (!defined("IN_ESOTALK")) exit;
 ET::$pluginInfo["Share"] = array(
 	"name" => "Share",
 	"description" => "Share conversations with Facebook, Twitter and Google+.",
-	"version" => "1.0.0",
+	"version" => "1.0.1",
 	"author" => "Tristan van Bokkem",
 	"authorEmail" => "tristanvanbokkem@gmail.com",
 	"authorURL" => "http://www.bitcoinclub.nl",
@@ -15,16 +15,18 @@ ET::$pluginInfo["Share"] = array(
 
 class ETPlugin_Share extends ETPlugin {
 
-
-	public function handler_conversationController_renderBefore($sender) {
+	// Add the Share controls CSS/JS to the conversation view.
+	public function handler_conversationController_renderBefore($sender)
+	{
 		$sender->addCSSFile($this->getResource("share.css"));
 		$sender->addJSFile($this->getResource("share.js"));
 		$sender->addJSLanguage("Share");
 	}
 
-	public function handler_conversationController_renderScrubberBefore($sender, $data) {
-		if(!ET::$session->user) return;
-		$controls = "<ul id='shareControls'>
+	// Display the Share controls above the scrubber.
+	public function handler_conversationController_renderScrubberBefore($sender, $data)
+	{
+		$controls = "<ul id='conversationShareControls'>
 						<li><a href='https://www.facebook.com/sharer/sharer.php?u=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-facebook'></i><span>".T("Share on")." Facebook</span></a></li>
 						<li><a href='https://twitter.com/share?text=".$data["conversation"]["title"]."&url=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-twitter'></i><span>".T("Share on")." Twitter</span></a></li>
 						<li><a href='https://plus.google.com/share?url=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-google-plus'></i><span>".T("Share on")." Google</span></a></li>
@@ -32,5 +34,4 @@ class ETPlugin_Share extends ETPlugin {
 
 		echo $controls;
 	}
-
 }
