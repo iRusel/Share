373b52f14d30dfc839eec53cf84fc1966eeb6659
iRusel <irusel@yandex.ru>
2015-07-28 22:46:13 +0300

    Share on VK
    Share on VK.com

--- plugin.php
+++ plugin.php
@@ -27,6 +27,7 @@ class ETPlugin_Share extends ETPlugin {
 	public function handler_conversationController_renderScrubberBefore($sender, $data)
 	{
 		$controls = "<ul id='conversationShareControls'>
+						<li><a href='https://vk.com/share.php?url=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-vk'></i><span>".T("Share on")." VK</span></a></li>
 						<li><a href='https://www.facebook.com/sharer/sharer.php?u=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-facebook'></i><span>".T("Share on")." Facebook</span></a></li>
 						<li><a href='https://twitter.com/share?text=".$data["conversation"]["title"]."&url=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-twitter'></i><span>".T("Share on")." Twitter</span></a></li>
 						<li><a href='https://plus.google.com/share?url=".URL(conversationURL($data["conversation"]["conversationId"], $data["conversation"]["title"]), true)."' target='_blank'><i class='icon-google-plus'></i><span>".T("Share on")." Google</span></a></li>
